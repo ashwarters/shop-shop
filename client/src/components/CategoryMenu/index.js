6abfbac1bb8139cc04d42501037b4224ef47f2f7
Ashley Warters <ashwarters@gmail.com>
2021-01-21 17:12:48 -0500

    state management set up

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useStoreContext } from "../../utils/GlobalState";
 import { useQuery } from '@apollo/react-hooks';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from "../../utils/queries";
 
 function CategoryMenu({ setCategory }) {
+  //calls useStoreContext() hook to retrieve the current state from the global state
+  //object and the disptach() method to update state
+  const [state, dispatch] = useStoreContext();
+
+  //destructuring state to only grab categories out of state
+  const { categories } = state;
+
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+
+
+  //useEffect() is a function that takes two arguments,
+  //a function to run given a certain condition, and then the condition
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating
+      // the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
 
   return (
     <div>
